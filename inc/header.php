36a8761b6205886f21ad51703b78e663ed9c602b
Ricardo Covarrubias <richard.cvbs.dev@gmail.com>
2022-03-30 14:24:28 -0600

    +navbar

--- inc/header.php
+++ inc/header.php
@@ -4,6 +4,38 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://bootswatch.com/5/cosmo/bootstrap.min.css">
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
     <title>Blog</title>
 </head>
 <body>
+
+
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container">
+    <a class="navbar-brand" href="#">Blogger</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarColor02">
+      <ul class="navbar-nav me-auto justify-content-evenly flex-grow-1">
+        <li class="nav-item">
+          <a class="nav-link lead" href="#">Home
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link lead" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link lead" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link lead" href="#">About</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
