b1b3f10f8528fc44aa1a60064a8f463451071a48
Ricardo Covarrubias <richard.cvbs.dev@gmail.com>
2022-03-30 19:57:06 -0600

    grammer correction

--- about.php
+++ about.php
@@ -11,7 +11,7 @@
 </div>
 <div class="container d-flex justify-content-center">
     <div class="card border-secondary mb-3" style="width: 60rem;">
-      <h1 class="card-header">You can Make new posts!</h1>
+      <h1 class="card-header">You can make new posts!</h1>
       <div class="card-body">
         <h4 class="card-title text-secondary">Made on php using MySQL (MariaDB)</h4>
         <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A sequi recusandae quasi commodi explicabo error consectetur esse minima labore possimus! Veniam laudantium sint sapiente vero quasi, est et esse quo similique. Non sed ea minima doloribus nisi, eveniet necessitatibus, totam pariatur libero autem saepe perferendis consectetur, id ipsam. Inventore iste magni, excepturi doloribus molestiae debitis recusandae necessitatibus repellendus possimus provident?</p>
